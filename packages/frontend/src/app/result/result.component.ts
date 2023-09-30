643c68b3426dfd791d84dac95a84956c8e8bc8a9
kpekala <kgpekala@gmail.com>
2023-09-30 22:56:40 +0200

    feat: add simple result screen

--- packages/frontend/src/app/result/result.component.ts
+++ packages/frontend/src/app/result/result.component.ts
@@ -9,4 +9,11 @@ export class ResultComponent {
   jobTitle = 'Pełnomocniczka ds. osób z niepełnosprawnościami';
   jobDescription = 'Działam na rzecz poprawy jakości życia osób z niepełnosprawnościami i na rzecz ich pełniejszego uczestnictwa w życiu społeczeństwa.';
   
+  menuItems = ['Opis zawodu', 'Specyfika pracy', 'Wymagania i umiejętności', 'Przedmioty szkolne'];
+
+  menuIndex = 0;
+
+  setItem(index: number) {
+    this.menuIndex = index;
+  }
 }
