2fa2b8ba3344e32716a5f965f3bf50b47c9a0a67
Dawid Bartosz <contact@dbartosz.com>
2023-09-30 15:31:57 +0200

    feat: add building and occupations scrapped (#3)

--- packages/scrapper/index.ts
+++ packages/scrapper/index.ts
@@ -1,31 +1,65 @@
 import 'dotenv/config';
 import axios from 'axios';
 import cheerio from 'cheerio';
+import fs from 'node:fs';
+import places from './DATA/places.json';
+import { getAllBuildings, getAllOccupations, mapJSONPlaceToKnownFormat } from './mappers/listPlaces';
 
 
-const BASE_URL = String(process.env.SITE_BASE_URL);
+const BASE_URL = 'https://mapakarier.org/_next/data/TFB5YHYlWb/miasto-zawodow/budynek/123/k.json?id=123&gender=k'
+// const BASE_URL = String(process.env.SITE_BASE_URL);
 
-async function fetchHTML(url: string): Promise<string> {
-    const { data } = await axios.get(url);
-    return data;
-}
+// async function fetchHTML(url: string): Promise<string> {
+//     const { data } = await axios.get(url);
+//     return data;
+// }
 
-async function scrape(): Promise<void> {
-    const html = await fetchHTML(BASE_URL);
+// async function scrape(): Promise<void> {
+//     const html = await fetchHTML(BASE_URL);
 
-    const $ = cheerio.load(html);
+//     // const $ = cheerio.load(html);
 
-    // For demonstration, let's scrape all the headers from the website
-    $('h1, h2, h3, h4, h5, h6').each((_idx, el) => {
-        const header = $(el).text();
-        console.log(header);
-    });
-}
+//     console.log(html);
 
-scrape()
-    .then(() => {
-        console.log("Scraping finished.");
-    })
-    .catch(error => {
-        console.error("An error occurred:", error);
-    });
+//     // console.log($.root()    );
+
+//     // For demonstration, let's scrape all the headers from the website
+//     // $('a').each((_idx, el) => {
+//     //     const header = $(el).text();
+//     //     console.log(header);
+//     // });
+// }
+
+// scrape()
+//     .then(() => {
+//         console.log("Scraping finished.");
+//     })
+//     .catch(error => {
+//         console.error("An error occurred:", error);
+//     });
+
+
+const allBuildings = getAllBuildings(places).map(mapJSONPlaceToKnownFormat);
+const allOcupations = getAllOccupations(places).map(mapJSONPlaceToKnownFormat);
+
+const SAVE_FOLDER = './DATA_FORMATTED';
+
+fs.writeFile(`${SAVE_FOLDER}/buildings.json`, JSON.stringify(allBuildings, null, 2), (err) => {
+    
+    if (err) {
+        return process.exit();
+    }
+
+    console.log('success');
+});
+
+
+
+fs.writeFile(`${SAVE_FOLDER}/occupations.json`, JSON.stringify(allOcupations, null, 2), (err) => {
+    
+    if (err) {
+        return process.exit();
+    }
+
+    console.log('success');
+});
--- packages/scrapper/links.ts
+++ packages/scrapper/links.ts
@@ -1,1 +1,8 @@
-export const linkToAllRoles = '/sciezki-kariery';
+// export const linkToAllRoles = '/sciezki-kariery';
+
+
+const baseUrl = 'https://mapakarier.org/_next/data/TFB5YHYlWb/miasto-zawodow';
+
+export const role = 'https://mapakarier.org/_next/data/TFB5YHYlWb/miasto-zawodow/zawod/114/k.json?id=114&gender=k';
+export const roles = 'https://mapakarier.org/_next/data/TFB5YHYlWb/miasto-zawodow.json';
+export const building = 'https://mapakarier.org/_next/data/TFB5YHYlWb/miasto-zawodow/budynek/123/k.json?id=123&gender=k';
