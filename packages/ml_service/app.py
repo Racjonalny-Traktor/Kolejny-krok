d3a3cc15145df430dff2b593db0bc105d81caa4d
ArkadiuszSlowik <arkadiusz.slowikk@gmail.com>
2023-10-01 09:32:33 +0200

    adds ml, flask microservice

---
+++ packages/ml_service/app.py
@@ -0,0 +1,204 @@
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
+
+import os
+import spacy
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pandas as pd
+import random
+import json
+
+def compare_context(text1, text2):
+
+    def preprocess_and_vectorize(text):
+        doc = nlp(text)
+        tokens = [token.text for token in doc if not token.is_stop and not token.is_punct]
+        clean_text = ' '.join(tokens)
+        vec = nlp(clean_text).vector
+        return vec
+
+    nlp = spacy.load("pl_core_news_md")
+
+    vec1 = preprocess_and_vectorize(text1)
+    vec2 = preprocess_and_vectorize(text2)
+
+    similarity = cosine_similarity([vec1], [vec2])[0][0]
+
+    return similarity
+
+def load_csv():
+    csv_filename = 'occupations.csv'
+    df = pd.read_csv(csv_filename)
+
+    return df
+
+def data_preparation(df):
+    del df['name'] # similar to fullName
+    del df['description'] # similar to shortDescription
+    # del df['jobDescription'] # lots of useless information, similar to shortDescription
+    # |del df['professionalStream'] # similar to fullName, and some nulls
+    del df['employers'] # feature not well implemented by mapa karier
+    del df['isRegulated'] # not relevant
+    del df['regulationDescription'] # useless, missing data
+    del df['simillarOccupations'] # not needed now, can be used in future to boost given groups of similar careers
+
+    df['marketSize'] = df['marketSize'].apply(_map_marketSize)
+    df['salary'] = df['salary'].apply(_map_salary)
+
+    column_mapping = {'math': 'matematyka',
+                      'biology': 'biologia',
+                      'physics': 'fizyka',
+                      'technics': 'technika',
+                      'polish': 'polski',
+                      'english': 'angielski',
+                      'geography': 'geografia',
+                      'history': 'historia',
+                      'wos': 'wiedza o społeczeństwie',
+                      'chemistry': 'chemia',
+                      'informatics': 'informatyka',
+                      'art': 'plastyka',
+                      'music': 'muzyka'
+                      }
+    df.rename(columns=column_mapping, inplace=True)
+
+    subject_list = column_mapping.values()
+    for subject in subject_list:
+        df[subject] = df[subject].apply(_map_subject, col_name=subject)
+    
+    df['futureJob'].apply(lambda x: 'Zawód przyszłości.' if x else '')
+    df['education'] = df['education'].fillna('')
+    
+    df.set_index('id', inplace=True)
+
+def _map_marketSize(value):
+    word_mapping = {
+        'ŚREDNI': '',
+        'MAŁY': '',
+        'DUŻY': 'Dużo miejsc pracy.',
+        'B.MAŁY': '',
+        'B.DUŻY': 'Dużo miejsc pracy.',
+         np.nan: ''
+        
+    }
+    return word_mapping.get(value, value)
+
+def _map_salary(value):
+    word_mapping = {
+        'B.DUŻE': 'Duże zarobki.',
+        'ŚREDNIE': '',
+        np.nan: '',
+        'DUŻE': 'Duże zarobki.',
+        'MAŁE': '',
+        'B.MAŁE': ''
+        
+    }
+    return word_mapping.get(value, value)
+
+def _map_subject(value, col_name):
+    word_mapping = {
+        1: '',
+        2: '',
+        3: f'Wymagany {col_name}.',
+        4: f'Wymagany {col_name}.',
+        
+    }
+    return word_mapping.get(value, value)
+
+def merge_columns(df):
+    merged_column = df.apply(lambda row: ' '.join(row.astype(str)), axis=1)
+    merged_df = pd.DataFrame({'description': merged_column})
+    merged_df['description'] = merged_df['description'].str.replace('\xa0', '')
+    merged_df['description'] = merged_df['description'].str.replace('\r', '')
+    merged_df['description'] = merged_df['description'].str.replace('\n', '')
+    merged_df['description'] = merged_df['description'].str.replace('--', '')
+    merged_df['description'] = merged_df['description'].str.replace('//', '')
+    merged_df.index = df.index
+
+    return merged_df
+
+def save_json(career_results):
+    file_name = "career_results.json"
+
+    with open(file_name, "w") as json_file:
+        # Use json.dump to write the dictionary to the file
+        json.dump(career_results, json_file)
+
+def load_json(file_name):
+    with open(file_name, "r") as json_file:
+        loaded_data = json.load(json_file)
+        return loaded_data
+
+def solve(career_results, df, question, answer):
+
+    for key, value in career_results.items():
+        random_number = random.random()
+
+        if random_number < 0.01:
+            career_results[key] = value + compare_context(df['description'][key], question) * answer
+
+    return career_results
+
+def return_solution(career_results, df_input):
+
+    top_3_careers = sorted(career_results, key=lambda k: career_results[k], reverse=True)[:3]
+
+    result = df_input[df_input['id'].isin(top_3_careers)]
+
+    return result
+
+def get_answer(value):
+    if value == 'tak':
+        return 1
+    elif value == 'raczej tak':
+        return 0.5
+    elif value == 'raczej nie':
+        return -0.5
+    elif value == 'nie':
+        return -1
+    else:
+        raise ValueError(f'There is no such answer as {value}!')
+    
+def get_question(id):
+    json_file = 'questions.json'
+    with open(json_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    for item in data:
+        if item.get('id') == id:
+            return item.get('question')
+    
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    data = request.get_json()
+
+    if not data or 'id' not in data or 'answer' not in data:
+        return jsonify({"error": "Both 'id' and 'answer' must be provided"}), 400
+
+    try:
+        df_input = load_csv()
+        df = df_input.copy()
+        data_preparation(df)
+        df = merge_columns(df)
+        career_results = load_json('career_results.json')
+        career_results = solve(career_results, df, get_question(data.question), get_answer(data.answer))
+        save_json(career_results)
+        result = return_solution(career_results, df_input)
+        json_result = result.to_json(orient='records')
+        return jsonify(json_result)
+    except ValueError:
+        return jsonify({"error": "Invalid input. Both 'id' and 'answer' must be integers"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
